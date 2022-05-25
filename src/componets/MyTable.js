a9b4b657b51cf54c240ed81f0869f66c70d8dba8
samu235 <samuel2351994@hotmail.es>
2022-05-25 12:48:11 +0200

    correction fail

--- src/componets/MyTable.js
+++ src/componets/MyTable.js
@@ -7,11 +7,11 @@ export default function MyTable(props) {
         let ret = true;
         if (props?.filter) {
             if (props?.filterType === "name") {
-                ret = (props?.filter.length <= 0 ||(item?.firstName.toUpperCase().indexOf(props?.filter) >= 0))
+                ret = (props?.filter.length <= 0 || (item?.firstName.toUpperCase().indexOf(props?.filter) >= 0))
             } else if (props?.filterType === "lastname") {
-                ret = (props?.filter.length <= 0 ||(item?.lastName.toUpperCase().indexOf(props?.filter) >= 0))
+                ret = (props?.filter.length <= 0 || (item?.lastName.toUpperCase().indexOf(props?.filter) >= 0))
             } else if (props?.filterType === "phone") {
-                ret = (props?.filter.length <= 0 ||(item?.phoneNumber.toString().indexOf(props?.filter) >= 0))
+                ret = (props?.filter.length <= 0 || (item?.phoneNumber.toString().indexOf(props?.filter) >= 0))
             }
             else {
                 ret = (props?.filter.length <= 0 || (item?.firstName.toUpperCase().indexOf(props?.filter) >= 0)
@@ -29,28 +29,28 @@ export default function MyTable(props) {
     return (
         <Main>
             <Table>
-
-                <Trhead>
-
-                    <Th>Rack</Th>
-                    <Th>Name</Th>
-                    <Th>Last Name</Th>
-                    <Th>Phone</Th>
-                </Trhead>
-                {
-                    props?.data
-                        ?.filter(data => booleanFilter(data))
-                        .map(data => {
-                            return (
-                                <Tr key={data?.userId} onClick={() => props?.setSelect(data)}>
-                                    <Td>{data?.userId}</Td>
-                                    <Td>{data?.firstName}</Td>
-                                    <Td>{data?.lastName}</Td>
-                                    <Td>{data?.phoneNumber}</Td>
-                                </Tr>
-                            )
-                        })
-                }
+                <tbody>
+                    <Trhead>
+                        <Th>Rack</Th>
+                        <Th>Name</Th>
+                        <Th>Last Name</Th>
+                        <Th>Phone</Th>
+                    </Trhead>
+                    {
+                        props?.data
+                            ?.filter(data => booleanFilter(data))
+                            .map(data => {
+                                return (
+                                    <Tr key={data?.userId} onClick={() => props?.setSelect(data)}>
+                                        <Td>{data?.userId}</Td>
+                                        <Td>{data?.firstName}</Td>
+                                        <Td>{data?.lastName}</Td>
+                                        <Td>{data?.phoneNumber}</Td>
+                                    </Tr>
+                                )
+                            })
+                    }
+                </tbody>
             </Table>
         </Main>
 
